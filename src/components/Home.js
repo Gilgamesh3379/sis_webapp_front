d9787d71d94b486dbc565b30cf88b3044f0c90a3
09196813 <rofl001@ess.ais.ac.nz>
2024-07-18 12:01:48 +1200

    home header removed from home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import '../App.css';
 
 function Home(props) {
     return (
-        <div className="Home-container">
-            <h1 className="Home-header">Welcome</h1>
-            <h1 className="Home-header">to</h1>
-            <h1 className="Home-header">Maungawhau Institute of Studies (MIS)</h1>
-            <h2 className="Home-subHeader">Homepage</h2>
+        <div className="">
+            <h1 >Welcome</h1>
+            <h1 >to</h1>
+            <h1 >Maungawhau Institute of Studies (MIS)</h1>
+            <h2 >Homepage</h2>
         </div>
     );
 }
